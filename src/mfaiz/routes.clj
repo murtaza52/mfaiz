392084eea9c8e88967a522a08bc1128c25047916
Murtaza Husain <murtaza.husain@searce.com>
2012-09-07 16:49:15 +0530

    route changes

--- src/mfaiz/routes.clj
+++ src/mfaiz/routes.clj
@@ -2,6 +2,7 @@
   (:use [net.cgrand.moustache :only [app]]
         [net.cgrand.enlive-html :only [deftemplate content html-content defsnippet]]
         [ring.middleware.file :only [wrap-file]]
+        [ring.middleware.stacktrace :only [wrap-stacktrace]]
         [ring.util.response :only [response]]))
 
 (defsnippet reg "mfaiz/templates/reg.html" [:#reg-form] [])
@@ -13,8 +14,9 @@
   [:#main] (content (reg)))
 
 (def my-app (app
+             wrap-stacktrace
              (wrap-file "resources/public/")
-             ["my"] (-> (reg) response constantly)
+             ["my"] (-> (my) response constantly)
              [page] (-> (chrome page) response constantly)
              [&] "Nothing was found"))
  
